23a20a971ebd63a1f833baf68faee0896c8bb28d
annaortegalsius <165768285+annaortegalsius@users.noreply.github.com>
2024-04-23 09:22:19 +0200

    es mou però no dispara

--- Escenes/Scripts/Avió.gd
+++ Escenes/Scripts/Avió.gd
@@ -14,15 +14,18 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("dreta", "esquerra")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if Input.is_action_pressed("dispara"):
 		dispara()
-
+		
 	move_and_slide()
-	
+
+var nou_laser
 func dispara():
-	var escena_laser:PackedScene= load("res://Escenes/escena_bales.tscn")
-	#nou_laser = escena_laser.instantiate 
+	var escena_laser:PackedScene= load("res://Escenes/escena_laser.tscn")
+	nou_laser= escena_laser.instantiate()
+	nou_laser.global_position = global_position
+	 
 	
