73bd1ae5530827000a0305df0d576b57f76c618d
emmamasso <165768272+emmamasso@users.noreply.github.com>
2024-04-23 09:42:33 +0200

    crear lasers i molts enemics

--- Escenes/Scripts/escena_principal.gd
+++ Escenes/Scripts/escena_principal.gd
@@ -1,9 +1,10 @@
 extends Node2D
 var EscenaPeons: PackedScene = load("res://Escenes/escena_peons.tscn")
 var nou_peo
+var posicio_inicial_peo = Vector2(116, 89)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	"$Timer.start()"
+	$Temps.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +13,6 @@ func _process(delta):
 
 func _on_timer_timeout():
 	nou_peo = EscenaPeons.instantiate()
-	$Timer.start()
+	$Temps.start()
+	nou_peo.position = posicio_inicial_peo
+	%Peons.add_child(nou_peo)
