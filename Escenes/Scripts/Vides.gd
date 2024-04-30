1c31a5415471f37de15a9a900a5c8081a4bad1ad
emmatarancon <165768213+emmatarancon@users.noreply.github.com>
2024-04-30 10:02:14 +0200

    he continuat el meteorit i quan et quedess sense vides va a la pantalla de gameover

--- Escenes/Scripts/Vides.gd
+++ Escenes/Scripts/Vides.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var Vida = 3
+var gameover:PackedScene = load("res://Escenes/has_perdut.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Vida==0:
+		get_tree().change_scene_to_packed(gameover)
