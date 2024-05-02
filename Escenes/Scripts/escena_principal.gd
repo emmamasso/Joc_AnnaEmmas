b49288ca8b65cc870d357d67a0b5f13b0bb5f545
emmatarancon <165768213+emmatarancon@users.noreply.github.com>
2024-05-02 08:56:57 +0200

    ja funcionen les vides

--- Escenes/Scripts/escena_principal.gd
+++ Escenes/Scripts/escena_principal.gd
@@ -1,15 +1,18 @@
 extends Node2D
 var EscenaPeons: PackedScene = load("res://Escenes/escena_peons.tscn")
+var game_over:PackedScene = load("res://Escenes/has_perdut.tscn")
 var nou_peo
 var posicio_inicial_peo = Vector2(41, 35)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Temps.start()
-
+	Vides.Vida=3
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Vides.Vida==0:
+		Vides.Vida=3
+		get_tree().change_scene_to_packed(game_over)
 
 func _on_timer_timeout():
 	nou_peo = EscenaPeons.instantiate()
