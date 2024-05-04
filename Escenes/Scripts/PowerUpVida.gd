3fca2c0579e6c63530f8db7ad2d5a9a629db1e31
emmatarancon <tarancon.emma@gmail.com>
2024-05-04 19:39:47 +0200

    ara dona un error que no se perque pero no us espanteu ja ho solucionare

--- Escenes/Scripts/PowerUpVida.gd
+++ Escenes/Scripts/PowerUpVida.gd
@@ -3,12 +3,12 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position=Vector2((randf_range(10,840)),-20)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position.y+=3
 
 
 func _on_body_entered(body):
