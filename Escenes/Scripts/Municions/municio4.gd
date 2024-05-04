28549f4b6d162056593f0b6d8279c07b532c2ad6
emmatarancon <tarancon.emma@gmail.com>
2024-05-04 16:22:55 +0200

    ja tinc casi tota la barra de munico

---
+++ Escenes/Scripts/Municions/municio4.gd
@@ -0,0 +1,14 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	color=Color(0,255,0,255)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Vides.bales<=1:
+		color = Color(255,0,0,255)
+	else:
+		color=Color(0,255,0,255)
