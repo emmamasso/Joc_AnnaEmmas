cf1aa23f80c93de9648fa676695b3ecc1fe9b194
emmatarancon <165768213+emmatarancon@users.noreply.github.com>
2024-05-07 09:46:39 +0200

    he ficat imatges dels lasers nobles

--- Escenes/Scripts/Menu.gd
+++ Escenes/Scripts/Menu.gd
@@ -3,13 +3,19 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	visible=false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if Input.is_action_just_pressed("escape"):
+		visible=true
+		get_tree().paused=true
 
 func _on_surt_pressed():
+	get_tree().quit()
+
+
+
+func _on_continua_pressed():
 	pass # Replace with function body.
