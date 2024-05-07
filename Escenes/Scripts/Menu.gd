74e53e419effe08c3220ca58e4d12a99c70b5fe3
emmatarancon <165768213+emmatarancon@users.noreply.github.com>
2024-05-07 10:03:29 +0200

    he avançat el  menu

--- Escenes/Scripts/Menu.gd
+++ Escenes/Scripts/Menu.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
-
-
+var pantallainici:PackedScene = load("res://Escenes/pantalla_inici.tscn")
+var escenaprinicpal:PackedScene = load("res://Escenes/escena_principal.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible=false
@@ -18,3 +18,11 @@ func _on_surt_pressed():
 func _on_continua_pressed():
 	visible=false
 	get_tree().paused=false
+
+
+func _on_pantalla_dinici_pressed():
+	get_tree().change_scene_to_packed(pantallainici)
+
+
+func _on_partidanova_pressed():
+	get_tree().change_scene_to_packed(escenaprinicpal)
