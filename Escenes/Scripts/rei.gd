44b26cf51326f371d32f632660edd16f299e8d84
emmatarancon <tarancon.emma@gmail.com>
2024-05-09 19:59:55 +0200

    he ficat el script del rei a scripys

--- Escenes/Scripts/rei.gd
+++ Escenes/Scripts/rei.gd
@@ -1,14 +1,25 @@
 extends CharacterBody2D
-var direccio:=Vector2 (0,0)
-var velocitat_màxima:=100
 
+const SPEED = 15.0
+@export var escena_bales :PackedScene
 
-func _ready():
-	pass
-	
-func _process(delta):
-	velocity= direccio.normalized()*velocitat_màxima
+func _physics_process(delta):
+	velocity.y = SPEED 
+	$Timer.start()
+
 	move_and_slide()
+var nova_bala
+func dispara():
+	var n_bales = 10
+	var direccio = Vector2.RIGHT
+	for i in range(n_bales):
+		nova_bala= escena_bales.instantiate()
+		Vides.Balespeons.add_child(nova_bala)
+		nova_bala.global_position = global_position
+		direccio = direccio.rotated(2*PI/n_bales)
+		nova_bala.direccio = direccio
 
 
-
+func _on_timer_timeout():
+	dispara()
+	
