5f80bce15e04ee38d73a4b429ec33591191e770b
emmatarancon <165768213+emmatarancon@users.noreply.github.com>
2024-05-10 14:35:25 +0200

    ja va el enter

--- Escenes/Scripts/pantalla_inici.gd
+++ Escenes/Scripts/pantalla_inici.gd
@@ -9,4 +9,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("enter"):
+		get_tree().change_scene_to_packed(escenaprinicpal)
